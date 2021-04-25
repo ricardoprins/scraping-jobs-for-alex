35ca2de71ad83496898648fc06d5aa1d4c29e331
Adnan Mansuri <adnanmansuri54@gmail.com>
2021-04-25 23:39:51 +0530

    Grabbing data from script tag (#2)
    * Grabbing data from the script tag instead of the HTML elements
    
    * Sample data (will revet this if not needed)
    
    * Fetching full description using threads
    
    * Sample data of 2018 jobs

--- src/ksl.py
+++ src/ksl.py
@@ -4,7 +4,7 @@ import requests
 import urllib3
 from random import randint
 from bs4 import BeautifulSoup
-
+from threading import Thread
 
 urllib3.disable_warnings()
 BASE_URL = "https://jobs.ksl.com/search/posted/last-7-days"
@@ -12,65 +12,62 @@ HEADERS = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
 }
 JOBS = {}
-names = []
-urls = []
-employers = []
-descriptions = []
+
+
+def getJobDescriptions(url, headers):
+
+    data = requests.get(url=url, headers=headers, verify=False, timeout=20)
+    data.close()
+    soup = BeautifulSoup(data.text, "html.parser")
+
+    descriptionTag = soup.find_all(
+        "meta", {"property": "og:description"}, "html.parser"
+    )
+
+    description = descriptionTag[0]["content"]
+    JOBS[url]["description"] = description
+
+
+def writeToFile():
+    global JOBS
+    with open("sample.json", "w") as outfile:
+        json.dump(JOBS, outfile)
 
 
 def getJobListings(url, headers):
-    MAIN_URL = "https://jobs.ksl.com"
     dataX = requests.get(url=url, headers=headers, verify=False, timeout=20)
     soup = BeautifulSoup(dataX.text, "html.parser")
+    dataX.close()
 
-    for title in soup.find_all("h2", {"class": "job-title"}):
-        anchor = title.find("a")
-        names.append(anchor.text.strip())
-        urls.append(anchor.get("href"))
+    script = soup.find_all('script', {'type': 'application/ld+json'})
+    content = script[0].contents[0]
 
-    for employer in soup.find_all("span", {"class": "company-name"}):
-        employers.append(employer.text.strip())
+    jobsArray = json.loads(content)["itemListElement"]
+    threads = []
+    for job in jobsArray:
+        JOBS[job["url"]] = {
+            "name": job["title"],
+            "employer": job["hiringOrganization"]["name"],
+            "url": job["url"],
+        }
+        t = Thread(target=getJobDescriptions, args=(job["url"], headers))
+        threads.append(t)
 
-    for url in urls:
-        newLink = MAIN_URL + url
-        getJobDescriptions(newLink, HEADERS)
-        time.sleep(randint(1, 15))
+    for i in threads:
+        i.start()
 
-    if (
-        len(names) == len(urls)
-        and len(urls) == len(employers)
-        and len(employers) == len(descriptions)
-    ):
-        for i in range(len(names)):
-            job = {
-                "name": names[i],
-                "employer": employers[i],
-                "url": urls[i],
-                "description": descriptions[i],
-            }
-            JOBS[urls[i]] = job
+    # Making sure all the jobs description is fetched
+    for i in threads:
+        i.join()
 
-    time.sleep(randint(1, 15))
+    print(f"Number of jobs Scraped {len(JOBS)}")
+
+    writeToFile()
+
     next_page = soup.find("a", {"class": "next link"})
 
     if next_page is not None:
         getJobListings(next_page.get("href"), HEADERS)
 
-    dataX.close()
-
-
-def getJobDescriptions(url, headers):
-    data = requests.get(url=url, headers=headers, verify=False, timeout=20)
-    soup = BeautifulSoup(data.text, "html.parser")
-
-    for description in soup.find_all(
-        "meta", {"property": "og:description"}, "html.parser"
-    ):
-        descriptions.append(description.text.strip())
-    data.close()
-
 
 getJobListings(BASE_URL, HEADERS)
-
-with open("sample.json", "w") as outfile:
-    json.dump(JOBS, outfile)
