ef02806e8017b180465cee8e79120675f41e9b01
Ricardo Prins <ricardoprins@gmail.com>
2023-05-18 23:17:32 -0600

    More efficiency updates, removal of dead files.

--- README.md
+++ README.md
@@ -8,8 +8,6 @@ _Alex is a guy who wants to know who is hiring who for what. Let's scrape jobs f
 
 Webscraping is fun. Webscraping also sits on the thin wall between legal and illegal. Webscraping can be hard!
 
-I'll begin a series of funny challenges aimed at displaying the intricate art of webscraping. I'll mostly use Python and several packages written for the purpose of webscraping.
-
-This repo will scrape jobs in Utah, initially from KSL and Indeed. I might include a statistical summary of the results. Oh, and a simple SQLite database will be used to store all the results.
+This repo will scrape jobs, initially from KSL and Indeed.
 
 That's it, let's scrape!
--- src/ksl.py
+++ src/ksl.py
@@ -1,5 +1,6 @@
 import json
 import requests
+import bs4
 from bs4 import BeautifulSoup
 from concurrent.futures import ThreadPoolExecutor
 from requests.exceptions import RequestException
@@ -9,21 +10,22 @@ HEADERS = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
 }
 
+KEYWORDS = ['software', 'developer', 'software engineer', 'programmer', 'data scientist', 'machine learning']
+
 class JobScraper:
     def __init__(self, base_url, headers):
         self.base_url = base_url
         self.headers = headers
         self.session = requests.Session()
         self.jobs = {}
+        self.keywords = KEYWORDS;
 
     def get_job_descriptions(self, url):
         try:
             data = self.session.get(url=url, headers=self.headers, timeout=20)
             soup = BeautifulSoup(data.text, "html.parser")
 
-            description_tag = soup.find_all(
-                "meta", {"property": "og:description"}, "html.parser"
-            )
+            description_tag = soup.find_all("meta", {"property": "og:description"})
 
             description = description_tag[0]["content"]
             self.jobs[url]["description"] = description
@@ -44,14 +46,15 @@ class JobScraper:
 
             jobs_array = json.loads(content)["itemListElement"]
             with ThreadPoolExecutor(max_workers=5) as executor:
-                executor.map(self.get_job_descriptions, [job["url"] for job in jobs_array])
-
-            for job in jobs_array:
-                self.jobs[job["url"]] = {
-                    "name": job["title"],
-                    "employer": job["hiringOrganization"]["name"],
-                    "url": job["url"],
-                }
+                for job in jobs_array:
+                    # Check if any of the keywords is in the job title
+                    if any(keyword.lower() in job["title"].lower() for keyword in self.keywords):
+                        self.jobs[job["url"]] = {
+                            "name": job["title"],
+                            "employer": job["hiringOrganization"]["name"],
+                            "url": job["url"],
+                        }
+                        executor.submit(self.get_job_descriptions, job["url"])
 
             print(f"Number of jobs Scraped {len(self.jobs)}")
 
@@ -59,7 +62,7 @@ class JobScraper:
 
             next_page = soup.find("a", {"class": "next link"})
 
-            if next_page is not None:
+            if next_page is not None and isinstance(next_page, bs4.Tag):
                 self.get_job_listings(next_page.get("href"))
         except RequestException as e:
             print(f"Error during requests to {url} : {str(e)}")
